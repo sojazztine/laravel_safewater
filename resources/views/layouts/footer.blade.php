fde66b883c0b582752920d3e6f836755160ab056
xtian <118583733+xtian082002@users.noreply.github.com>
2025-03-25 16:54:33 +0800

    adding some details in footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,41 +1,43 @@
 <footer class="min-h-[300px] bg-[#016262]">
     <div class="flex justify-end px-10 pt-10">
-        <img src="../img/img-footer/footer-restore.png" alt="">
+        <img src="../img/img-footer/footer-restore.png" alt="" class="max-w-[150px] md:max-w-[200px]">
     </div>
 
     <div class="flex justify-center">
-        <div class="flex text-white w-[85%]">
-            <div class="flex flex-col p-10">
-                <h1 class="font-semibold mb-5 text-xl">
-                    Solutions
-                </h1>
-                <h1 href="#" class="mb-3 text-lg">
-                    Community Collections
-                </h1>
-                <h1 href="#" class="mb-3 text-lg">
-                    Restore Boards
-                </h1>
-                <h1 href="#" class="mb-3 text-lg">
-                    Restore Furniture and Applications
-                </h1>
-                <h1 href="#" class="mb-3 text-lg">
-                    Restore-a-Classroom
-                </h1>
+        <div class="w-[85%] text-white grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10">
+            <div class="flex flex-col p-5 md:p-10">
+                <h1 class="font-semibold mb-4 text-lg md:text-xl">Solutions</h1>
+                <a href="" class="mb-2 text-md hover:underline">Community Collections</a>
+                <a href="" class="mb-2 text-md hover:underline">Restore Boards</a>
+                <a href="" class="mb-2 text-md hover:underline">Restore Furniture and Applications</a>
+                <a href="" class="mb-2 text-md hover:underline">Restore-a-Classroom</a>
             </div>
-            <div class="flex flex-col p-10">
-                <h1 class="font-semibold mb-5 text-xl">
-                    About Us
-                </h1>
-                <h1 class="font-semibold text-xl">
-                    Contact Us
-                </h1>
+
+            <div class="flex flex-col p-5 md:p-10">
+                <h1 class="font-semibold mb-4 text-lg md:text-xl">About Us</h1>
+                <a href="" class="mb-2 text-md hover:underline">About The Company</a>
+                <a href="" class="mb-2 text-md hover:underline">Mission // Vision</a>
+                <a href="" class="mb-2 text-md hover:underline">The Team</a>
+                <a href="" class="mb-2 text-md hover:underline">The EPR Act</a>
+                <a href="" class="mb-2 text-md hover:underline">Community Collections</a>
+            </div>
+
+            <div class="flex flex-col p-5 md:p-10">
+                <h1 class="font-semibold mb-4 text-lg md:text-xl">Contact Us</h1>
+                <a href="" class="mb-2 text-md hover:underline">Email</a>
+                <a href="" class="mb-2 text-md hover:underline">Facebook</a>
+                <a href="" class="mb-2 text-md hover:underline">Instagram</a>
+                <a href="" class="mb-2 text-md hover:underline">Tiktok</a>
+                <a href="" class="mb-2 text-md hover:underline">LinkedIn</a>
             </div>
         </div>
     </div>
 
     <div class="flex justify-center pb-10">
-        <div class="border-t w-[85%] flex justify-end">
-            <h1 class="mt-3 text-white">© 2025 Restore Solutions PH. All rights reserved.</h1>
+        <div class="border-t w-[85%] flex justify-center md:justify-end">
+            <h1 class="mt-3 text-white text-center md:text-right text-sm md:text-base">
+                © 2025 Restore Solutions PH. All rights reserved.
+            </h1>
         </div>
     </div>
 </footer>
