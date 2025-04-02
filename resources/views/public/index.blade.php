061fee9026cf0c7d9e267499d5cfa8f6adaa570a
xtian <118583733+xtian082002@users.noreply.github.com>
2025-04-02 14:53:25 +0800

    fixing testimonial

--- resources/views/public/index.blade.php
+++ resources/views/public/index.blade.php
@@ -1,8 +1,5 @@
-
-
-
 <x-public-layout>
-<!--
+    <!--
 <style>
 @keyframes bgAnimation {
     0%{
@@ -61,18 +58,21 @@
         <!-- Overlay -->
         <div class="absolute inset-0 bg-black/40 z-10"></div>
         <div class="relative h-56 md:h-96">
-        <!-- Carousel wrapper -->
-            @foreach ($landingPages as $landingPage )
-                    <div class="absolute inset-0 flex transition-opacity duration-1000 ease-in-out opacity-0" data-carousel-item>
-                        <img src="{{Storage::url($landingPage->image) }}" class="w-full h-screen object-cover z-0" alt="...">
-                        </div>
+            <!-- Carousel wrapper -->
+            @foreach ($landingPages as $landingPage)
+                <div class="absolute inset-0 flex transition-opacity duration-1000 ease-in-out opacity-0"
+                    data-carousel-item>
+                    <img src="{{ Storage::url($landingPage->image) }}" class="w-full h-screen object-cover z-0"
+                        alt="...">
+                </div>
             @endforeach
 
-          </div>
+        </div>
 
-        <div class="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center text-center">
-                <h1 class="text-white font-bold text-7xl">{{$hero_heading_title}}</h1>
-                <h3 class="text-white text-3xl">{{$hero_heading_description}}</h3>
+        <div
+            class="absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col items-center text-center">
+            <h1 class="text-white font-bold text-7xl">{{ $hero_heading_title }}</h1>
+            <h3 class="text-white text-3xl">{{ $hero_heading_description }}</h3>
 
         </div>
     </div>
@@ -123,11 +123,13 @@
             </p>
 
             <div class="relative">
-                <img src="img/img-index/pieceCount.png" alt="Piece Count Image" class="hidden lg:block max-w-full h-auto">
+                <img src="img/img-index/pieceCount.png" alt="Piece Count Image"
+                    class="hidden lg:block max-w-full h-auto">
 
                 <div class="absolute bottom-0 left-0 w-full flex items-center justify-between p-4">
                     <h1 class="text-white text-xl font-bold">Every piece of plastic counts</h1>
-                    <button class="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black shadow-md">
+                    <button
+                        class="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black shadow-md">
                         ➝
                     </button>
                 </div>
@@ -145,11 +147,13 @@
             </p>
 
             <div class="relative">
-                <img src="img/img-index/RestoreBoards.png" alt="Smart Eco Bin Image" class="hidden lg:block max-w-full h-auto">
+                <img src="img/img-index/RestoreBoards.png" alt="Smart Eco Bin Image"
+                    class="hidden lg:block max-w-full h-auto">
 
                 <div class="absolute bottom-0 left-0 w-full flex items-center justify-between p-4">
                     <h1 class="text-white text-xl font-bold">Restore Boards</h1>
-                    <button class="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black shadow-md">
+                    <button
+                        class="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black shadow-md">
                         ➝
                     </button>
                 </div>
@@ -167,11 +171,13 @@
             </p>
 
             <div class="relative">
-                <img src="img/img-index/RestoreFurnitureandApplications.png" alt="Circular Economy Image" class="hidden lg:block max-w-full h-auto">
+                <img src="img/img-index/RestoreFurnitureandApplications.png" alt="Circular Economy Image"
+                    class="hidden lg:block max-w-full h-auto">
 
                 <div class="absolute bottom-0 left-0 w-full flex items-center justify-between p-4">
                     <h1 class="text-white text-xl font-bold">Restore Furniture and Applications</h1>
-                    <button class="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black shadow-md">
+                    <button
+                        class="w-10 h-10 flex items-center justify-center rounded-full bg-white text-black shadow-md">
                         ➝
                     </button>
                 </div>
@@ -189,14 +195,20 @@
             <div class="hidden md:block">
                 <img src="img/img-index/restoreMachine.png" alt="">
             </div>
-            <div class="mb-10">
+            <div class="">
                 <h1 class="md:text-6xl text-2xl font-bold text-[#016262] mb-10">Smart EcoBin</h1>
-                <p class="md:text-2xl text-lg mb-6 md:leading-10 text-[#016262] font-normal">Anybody can be a hero. <br> Help lead the charge towards a cleaner <br> and greener future!</p>
-                <p class="md:text-2xl text-lg mb-6 md:leading-10 text-[#016262] font-normal">Every piece of plastic counts!</p>
-                <p class="md:text-2xl text-lg mb-20 md:leading-10 text-[#016262] font-normal ">Sign-up for your Smart E-Collection Bin <br> account now and help make a difference!</p>
+                <p class="md:text-2xl text-lg mb-6 md:leading-10 text-[#016262] font-normal">Anybody can be a hero. <br>
+                    Help lead the charge towards a cleaner <br> and greener future!</p>
+                <p class="md:text-2xl text-lg mb-6 md:leading-10 text-[#016262] font-normal">Every piece of plastic
+                    counts!</p>
+                <p class="md:text-2xl text-lg mb-20 md:leading-10 text-[#016262] font-normal ">Sign-up for your Smart
+                    E-Collection Bin <br> account now and help make a difference!</p>
                 <div class="flex gap-[1vw] flex-col md:flex-row">
-                    <a href="" class="bg-[#016262] rounded-full   md:px-10 py-2 px-4 text-center text-white border hover:text-[#016262] hover:border-[#016262] hover:bg-[#f9fff5] transition delay-30 duration-300 ease-in-out">Login</a>
-                    <a href="" class="bg-[#51A5BE] rounded-full   md:px-10 py-2 px-4 text-center text-white border hover:text-[#51A5BE] hover:border-[#51A5BE] hover:bg-[#f9fff5] transition delay-30 duration-300 ease-in-out">Register Now</a>
+                    <a href=""
+                        class="bg-[#016262] rounded-full   md:px-10 py-2 px-4 text-center text-white border hover:text-[#016262] hover:border-[#016262] hover:bg-[#f9fff5] transition delay-30 duration-300 ease-in-out">Login</a>
+                    <a href=""
+                        class="bg-[#51A5BE] rounded-full   md:px-10 py-2 px-4 text-center text-white border hover:text-[#51A5BE] hover:border-[#51A5BE] hover:bg-[#f9fff5] transition delay-30 duration-300 ease-in-out">Register
+                        Now</a>
                 </div>
                 <h1 class="text-2xl text-[#016262] mt-10 font-semibold">Visit our SEB Mall Locations</h1>
                 <p class="text-[#016262] ">Visit our SEB Mall Locations Near You!</p>
@@ -251,9 +263,9 @@
                 Amount of Sachets Collected
             </h1>
 
-                    <h1 class="text-[#016262] text-9xl font-bold mb-10 lg:w-[580px] italic">
-                    {{ number_format($total_sachets)  }}
-                    </h1>
+            <h1 class="text-[#016262] text-9xl font-bold mb-10 lg:w-[580px] italic">
+                {{ number_format($total_sachets) }}
+            </h1>
 
             <h1 class="text-[#016262] text-4xl font-semibold lg:w-[470px] leading-14 mb-5">
                 Diverted as of today
@@ -261,7 +273,8 @@
         </div>
 
         <div class="">
-            <img src="img/bulb.png" width="400" alt="" class="rounded-tr-[300px] rounded-tl-lg rounded-b-lg hidden lg:block">
+            <img src="img/bulb.png" width="400" alt=""
+                class="rounded-tr-[300px] rounded-tl-lg rounded-b-lg hidden lg:block">
         </div>
     </div>
 
@@ -276,16 +289,24 @@
         <div class=" flex flex-col items-center">
             <img src="img/img-index/presenting.png" alt="Presenting Image" class="hidden lg:block mb-10">
             <div class="flex">
-                <a href="" class="me-3 text-white bg-[#016262] py-3 px-12 rounded-full text-xl hover:border-[#016262] hover:text-[#016262] hover:bg-[#f9fff5] border border-transparent transition delay-30 duration-300 ease-in-out  ">Watch our Videos</a>
-                <a href="" class=" text-white bg-[#016262] py-3 px-12 rounded-full text-xl hover:border-[#016262] hover:text-[#016262] hover:bg-[#f9fff5] border border-transparent transition delay-30 duration-300 ease-in-out  ">Join the Movement</a>
+                <a href=""
+                    class="me-3 text-white bg-[#016262] py-3 px-12 rounded-full text-xl hover:border-[#016262] hover:text-[#016262] hover:bg-[#f9fff5] border border-transparent transition delay-30 duration-300 ease-in-out  ">Watch
+                    our Videos</a>
+                <a href=""
+                    class=" text-white bg-[#016262] py-3 px-12 rounded-full text-xl hover:border-[#016262] hover:text-[#016262] hover:bg-[#f9fff5] border border-transparent transition delay-30 duration-300 ease-in-out  ">Join
+                    the Movement</a>
             </div>
         </div>
 
         <div class="flex justify-center">
             <div class="w-[75%]">
-                <h1 class="text-4xl text-[#016262] mb-8 font-bold w-[50%]">Be part of the change you want to see in our environment!</h1>
-                <p class="text-[#016262] text-2xl lg:w-[600px] leading-8 font-medium">Since the pollution problem is everybody’s problem, We believe that our goal of solving the problem of plastic pollution will be a lot easier to achieve through a collective effort.</p>
-                <p class="mt-5 mb-20 text-[#016262] text-2xl lg:w-145 leading-8 font-medium">We are stronger when we are together.</p>
+                <h1 class="text-4xl text-[#016262] mb-8 font-bold w-[50%]">Be part of the change you want to see in our
+                    environment!</h1>
+                <p class="text-[#016262] text-2xl lg:w-[600px] leading-8 font-medium">Since the pollution problem is
+                    everybody’s problem, we believe that our goal of solving the problem of plastic pollution will be a
+                    lot easier to achieve through a collective effort.</p>
+                <p class="mt-5 mb-20 text-[#016262] text-2xl lg:w-145 leading-8 font-medium">We are stronger when we
+                    are together.</p>
             </div>
         </div>
 
@@ -300,7 +321,7 @@
     <!-- PARTNERS -->
     <div class="mt-[150px]">
         <div class="flex justify-center mb-[10px]">
-            <h1 class="text-bolder text-2xl text-[#016262]">
+            <h1 class="text-bolder font-bold mb-5 text-2xl text-[#016262]">
                 Our partners
             </h1>
         </div>
@@ -321,48 +342,50 @@
 
 
 
-   @php
-    $limit = request()->query('limit');
-@endphp
+    @php
+        $limit = request()->query('limit');
+    @endphp
 
-<!-- TESTIMONIALS -->
-<div class="min-h-[500px] bg-[#016262] mt-20 mb-20 pb-10">
-    <div class="ml-20 pt-20">
-        <h1 class="text-5xl font-bold text-[#EBFCFC]">
-            Words from our Eco Heroes
-        </h1>
+    <!-- TESTIMONIALS -->
+    <div class="min-h-[500px] bg-[#016262] mt-20 mb-20 pb-10">
+        <div class="ml-20 pt-20">
+            <h1 class="text-5xl font-bold text-[#EBFCFC]">
+                Words from our Eco Heroes
+            </h1>
 
-        <!-- Limit only the images to 3 -->
-        <div class="flex items-center mt-10">
-            @foreach ($testimonials->sortByDesc('created_at')->take(3) as $testimonial)
-                <img src="{{ Storage::url($testimonial->image) }}" alt="Testimonial Image"
-                    class="border-4 rounded-full w-[70px] h-[70px] border-[#17B67D] border-solid ml-[-15px]">
-            @endforeach
-            <p class="ml-10 text-xl text-[#EBFCFC] lg:w-120 lg:block hidden">
-                Latest testimonials.
-            </p>
-            <p class="text-[#EBFCFC] lg:w-120 mt-10 block md:hidden text-xl">
-                Latest testimonials
-             </p>
-        </div>
-
-
-
-        <div class="mt-10 flex flex-wrap justify-center gap-[3vw] p-3">
-            @foreach ($testimonials as $testimonial)
-            <div class="w-80 h-90 rounded-xl bg-[#EBFCFC] p-5">
-                <img src="images/testimonial/{{Storage::url($testimonial->image)}}" alt="Testimonial Picture" class="border-4 w-15 rounded-full border-[#17B67D]  border-solid ">
-                <p class="text-[#016262] break-words mt-2 mb-5">{{ $testimonial->content }}</p>
-                <p class="text-[#016262] ">{{$testimonial->name}}</p>
-                <p class="text-[#016262]">{{$testimonial->company}}</p>
+            <!-- Limit only the images to 3 -->
+            <div class="flex items-center mt-10">
+                @foreach ($testimonials->sortByDesc('created_at')->take(3) as $testimonial)
+                    <img src="{{ Storage::url($testimonial->image) }}" alt="Testimonial Image"
+                        class="border-4 rounded-full w-[70px] h-[70px] border-[#17B67D] border-solid ml-[-15px]">
+                @endforeach
+                <p class="ml-10 text-xl text-[#EBFCFC] lg:w-120 lg:block hidden">
+                    Latest testimonials.
+                </p>
+                <p class="text-[#EBFCFC] lg:w-120 mt-10 block md:hidden text-xl">
+                    Latest testimonials
+                </p>
             </div>
-             @endforeach
 
 
 
+            <div class="mt-10 flex flex-wrap justify-center gap-[3vw] p-3">
+                @foreach ($testimonials as $testimonial)
+                    <div class="w-80 h-90 rounded-xl bg-[#EBFCFC] p-5">
+                        <img src="images/testimonial/{{ Storage::url($testimonial->image) }}"
+                            alt="Testimonial Picture"
+                            class="border-4 w-15 rounded-full border-[#17B67D]  border-solid ">
+                        <div class="h-40 overflow-auto p-2 my-5">
+                            <p class="text-[#016262] break-words mt-2 mb-5">{{ $testimonial->content }}</p>
+                        </div>
+                        <p class="text-[#016262] ">{{ $testimonial->name }}</p>
+                        <p class="text-[#016262]">{{ $testimonial->company }}</p>
+                    </div>
+                @endforeach
+
+            </div>
         </div>
     </div>
-</div>
 
 
 
