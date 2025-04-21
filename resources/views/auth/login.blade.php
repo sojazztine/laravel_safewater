e9273a76e848fd986a4da4ea605f13c0c5798293
xtian <118583733+xtian082002@users.noreply.github.com>
2025-04-21 21:52:58 +0800

    fixing login interface

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,14 +12,14 @@
 
         <!-- Email Address -->
         <div>
-            <x-input-label for="email" :value="__('Email')" />
+            <x-input-label for="email" :value="__('Email*')" />
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
         <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+            <x-input-label for="password" :value="__('Password*')" />
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -37,14 +37,14 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+        <div class="flex flex-col items-center mt-4">
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 mt-10">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
